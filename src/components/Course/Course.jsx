7dab3e97ca824e7da83a823a3eb1154ad994cbae
selimRU <selim.soha92@gmail.com>
2023-09-15 14:26:05 +0600

    provided course props and  displayed all data in course componet

---
+++ src/components/Course/Course.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FiDollarSign } from 'react-icons/fi';
+import { BiBookOpen } from 'react-icons/bi';
+const Course = ({ course }) => {
+    const { title, description, credit, price, img } = course
+    return (
+        <div className='px-5 bg-white shadow-md py-5 rounded-md space-y-4'>
+            <img className='w-full lg:hover:-translate-y-1 hover:scale-110' src={img} alt="" />
+            <h3 className='text-[18px] font-semibold'>{title}</h3>
+            <p className=' text-sm text-gray-500 font-normal'>{description.length > 150 ? description.slice(0, 150) : description}</p>
+            <div className='flex justify-between items-center text-gray-500 font-light text-base'>
+                <div className='flex items-center gap-3'>
+                    <FiDollarSign />
+                    <p>Price: {price}</p>
+                </div>
+                <div className='flex items-center gap-3'>
+                    <BiBookOpen />
+                    <p>Credit: {credit} hr</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Course;
