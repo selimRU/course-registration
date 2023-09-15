4c4e840735aa2197ac4c692e651c518a137926c7
selimRU <selim.soha92@gmail.com>
2023-09-15 14:02:51 +0600

    created components and all data kept in public file

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Courses from "./components/Courses/Courses"
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className=" text-[32px] font-bold text-center container mx-auto py-10">Course Registration</h1>
+      <div className=" px-2 lg:px-10">
+        <Courses></Courses>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",],
+  daisyui: {
+    themes: [
+
+      "garden",
+    ],
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
+
